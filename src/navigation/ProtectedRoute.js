d1370536232f66b3fb28a6310af3611969ac5960
edwardsuwirya <edward_suwirya@yahoo.com>
2023-01-02 17:34:22 +0700

    Change protected route when success react component

--- src/navigation/ProtectedRoute.js
+++ src/navigation/ProtectedRoute.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {useSelector} from "react-redux";
-import {Navigate, Outlet} from "react-router-dom";
+import {Navigate} from "react-router-dom";
 import {NAVIGATION} from "../constants";
+import DashboardPage from "../pages/DashboardPage";
 
-const ProtectedRoute = () => {
+const ProtectedRoute = ({service, localStorage}) => {
     const userState = useSelector(state => state.userCredentialReducer);
-    return userState.userInfo !== null ? <Outlet/> : <Navigate to={NAVIGATION.INDEX.path} replace/>;
+    return userState.userInfo !== null ? <DashboardPage service={service} localStorage={localStorage}/> :
+        <Navigate to={NAVIGATION.INDEX.path} replace/>;
 };
 
 export default ProtectedRoute;
