6be962956cb6c79b84066c69619a109406dbde38
edwardsuwirya <edward_suwirya@yahoo.com>
2023-01-02 17:08:06 +0700

    Add protected route

--- src/navigation/routerFactory.js
+++ src/navigation/routerFactory.js
@@ -6,6 +6,7 @@ import LoginPage from "../pages/LoginPage";
 import ErrorPage from "../pages/ErrorPage";
 import CoursePage from "../pages/CoursePage";
 import CourseTypePage from "../pages/CourseTypePage";
+import ProtectedRoute from "./ProtectedRoute";
 
 
 export const routerFactory = () => createBrowserRouter(
@@ -19,8 +20,9 @@ export const routerFactory = () => createBrowserRouter(
                 // {index: true, element: <DashboardPage service={services.authService}/>},
                 {
                     path: NAVIGATION.HOME_ROUTE.path,
-                    element: <DashboardPage/>,
+                    element: <ProtectedRoute/>,
                     children: [
+                        {index: true, element: <DashboardPage/>},
                         {
                             path: NAVIGATION.COURSE_ROUTE.path,
                             element: <CoursePage/>,
--- src/pages/DashboardPage/components/HeaderUserInfo/HeaderUserInfo.js
+++ src/pages/DashboardPage/components/HeaderUserInfo/HeaderUserInfo.js
@@ -1,22 +1,12 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {postLogout} from "../../../../state/userCredential/userCredentialAction";
 import {connect} from "react-redux";
-import {useNavigate} from "react-router-dom";
-import {NAVIGATION} from "../../../../constants";
 import {LogoutLink, UserInfoContainer} from "./HeaderUserInfoStyle";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {useDeps} from "../../../../utils/hooks/useDeps";
 
-// Using mapStateToPros and mapDispatchToProps, example purpose only
-// Another technique using useSelector or useDispatch
 const HeaderUserInfo = ({userState, postLogout}) => {
     const {services, localstorage} = useDeps();
-    const onNavigate = useNavigate();
-    useEffect(() => {
-        if (!userState.userInfo) {
-            onNavigate(NAVIGATION.INDEX.path);
-        }
-    }, [userState])
     const handleLogout = () => {
         postLogout(services.authService.doLogout, localstorage);
     }
