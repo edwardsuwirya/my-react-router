7a7e0424c5c0e1e3fac688abc180fd1baea03376
edwardsuwirya <edward_suwirya@yahoo.com>
2023-01-02 17:34:22 +0700

    Change protected route when success react component

--- src/navigation/routerFactory.js
+++ src/navigation/routerFactory.js
@@ -19,9 +19,8 @@ export const routerFactory = (services) => createBrowserRouter(
                 {index: true, element: <LoginPage service={services.authService}/>},
                 {
                     path: NAVIGATION.HOME_ROUTE.path,
-                    element: <ProtectedRoute/>,
+                    element: <ProtectedRoute service={services.authService}/>,
                     children: [
-                        {index: true, element: <DashboardPage service={services.authService}/>},
                         {
                             path: NAVIGATION.COURSE_ROUTE.path,
                             element: <CoursePage/>,
